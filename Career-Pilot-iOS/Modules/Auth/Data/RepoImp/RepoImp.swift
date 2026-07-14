fdee2d984919bc55d0f197281ceb2b11fac3f89e
Mohamed Magdy <88778969+mohamedmagdyoo@users.noreply.github.com>
2026-07-14 15:09:24 +0300

    Setup Folder structure

---
+++ Career-Pilot-iOS/Modules/Auth/Data/RepoImp/RepoImp.swift
@@ -0,0 +1,8 @@
+//
+//  RepoImp.swift
+//  Career-Pilot-iOS
+//
+//  Created by Mohamed Magdy on 14/07/2026.
+//
+
+import Foundation
---
+++ Career-Pilot-iOS/Modules/Auth/Presentation/VM/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  Career-Pilot-iOS
+//
+//  Created by Mohamed Magdy on 14/07/2026.
+//
+
+import Foundation
