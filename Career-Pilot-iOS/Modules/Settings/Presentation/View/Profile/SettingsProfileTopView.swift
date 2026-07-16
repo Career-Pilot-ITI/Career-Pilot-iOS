021f4b2950473319e17d2eab7b6ff71fafa27d78
Eyad waleed <eyadwaleedabdelfatah@gmail.com>
2026-07-16 17:50:06 +0300

    the top view is ready

---
+++ Career-Pilot-iOS/Modules/Settings/Presentation/View/Profile/SettingsProfileTopView.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsProfileTopView.swift
+//  Career-Pilot-iOS
+//
+//  Created by Eyad waleed on 16/07/2026.
+//
+
+import SwiftUI
+
+struct SettingsProfileTopView: View {
+    var onSave: () -> Void
+    var body: some View {
+        HStack{
+            Text("Edit Profile")
+            Spacer() 
+            Button(action: {
+                onSave()
+                
+            }){
+                Text("Save").foregroundColor(.gray200)
+                
+            }
+        }
+        
+        
+    }
+}
+
+//struct SettingsProfileTopView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SettingsProfileTopView()
+//    }
+//}
