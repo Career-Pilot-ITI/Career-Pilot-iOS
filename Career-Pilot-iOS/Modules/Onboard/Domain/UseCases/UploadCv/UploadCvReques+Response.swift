d6bb0fc5d8a5cc1b1227ae9130f2df1964dea6c3
Mohamed Magdy <88778969+mohamedmagdyoo@users.noreply.github.com>
2026-07-17 16:11:39 +0300

    Finish Domain layer

---
+++ Career-Pilot-iOS/Modules/Onboard/Domain/UseCases/UploadCv/UploadCvReques+Response.swift
@@ -0,0 +1,16 @@
+//
+//  UploadCvReques+Response.swift
+//  Career-Pilot-iOS
+//
+//  Created by Mohamed Magdy on 17/07/2026.
+//
+
+import Foundation
+
+struct UploadCvRequest{
+    var cv: URL
+}
+
+struct UploadCvResponse{
+    var message: String
+}
---
+++ Career-Pilot-iOS/Modules/Onboard/Domain/UseCases/UploadCv/UploadCvUseCase.swift
@@ -0,0 +1,25 @@
+//
+//  UploadCvUseCase.swift
+//  Career-Pilot-iOS
+//
+//  Created by Mohamed Magdy on 17/07/2026.
+//
+
+import Foundation
+
+protocol UseCase{
+    associatedtype Input
+    associatedtype Output
+    
+    func excute(input: Input) async throws -> Output
+}
+
+class UploadCvUseCase: UseCase{
+
+    typealias Input = UploadCvRequest
+    typealias Output = UploadCvResponse
+    
+    func excute(input: UploadCvRequest) async throws -> UploadCvResponse {
+        <#code#>
+    }
+}
