8881a84ff7ccd06487631590b20dbe467d11b01b
Moaz Osama <moazosama204@gmail.com>
2026-07-17 05:45:32 -0700

    add usecase generic

--- Career-Pilot-iOS/Modules/Auth/Domain/UseCases/UseCases.swift
+++ Career-Pilot-iOS/Modules/Auth/Domain/UseCases/UseCases.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol UseCase {
+    associatedtype Input
+    associatedtype Output
+    
+    func execute(_ input: Input) async throws -> Output
+}
+
+class SendOTPUseCase : UseCase {
+    private let repository: AuthRemoteDataSourceProtocol
+    
+    init(repository: AuthRemoteDataSourceProtocol) {
+        self.repository = repository
+    }
+    
+    func execute(_ input: String) async throws {
+        try await repository.sendOTP(for: input)
+    }
+    
+}
