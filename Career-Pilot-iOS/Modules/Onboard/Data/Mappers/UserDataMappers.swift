13d8b00eb5dcd9087588d0a45c41a0029e5991db
Mohamed Magdy <88778969+mohamedmagdyoo@users.noreply.github.com>
2026-07-17 19:55:32 +0300

    Finish The Data User

---
+++ Career-Pilot-iOS/Modules/Onboard/Data/Mappers/UserDataMappers.swift
@@ -0,0 +1,14 @@
+//
+//  UserDataMappers.swift
+//  Career-Pilot-iOS
+//
+//  Created by Mohamed Magdy on 17/07/2026.
+//
+
+import Foundation
+
+extension CvUplodingApiResponse{
+    func mapToEntity() -> UploadCvResponse{
+        return UploadCvResponse(message: "Mapping")
+    }
+}
