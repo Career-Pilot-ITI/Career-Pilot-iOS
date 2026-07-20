f717f90a296f45ef03e540c061d4631a904b909f
AhmedElSayyadMohamed <elsayyada197@gmail.com>
2026-07-20 18:27:29 +0300

    chore(cleanup): remove duplicate network files
    - Consolidate network services into NetworkService/ directory
    - Remove old TokenProvider.swift, NetworkError.swift at root level

--- Career-Pilot-iOS/Core/Network/AuthenticatedNetworkService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AuthenticatedNetworkService.swift
-//  Career-Pilot-iOS
-//
-//  Created by Ahmed El-Sayyad Mohamed on 20/07/2026.
-//
-
-import Foundation
--- Career-Pilot-iOS/Core/Network/TokenProvider.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TokenProvider.swift
-//  Career-Pilot-iOS
-//
-//  Created by Ahmed El-Sayyad Mohamed on 20/07/2026.
-//
-
-import Foundation
