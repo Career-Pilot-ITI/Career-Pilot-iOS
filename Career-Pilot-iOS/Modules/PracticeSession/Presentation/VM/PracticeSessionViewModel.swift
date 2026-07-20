95b4dabcd0debbd67c27383514c77cd30f9b5e3e
Mohamed Magdy <88778969+mohamedmagdyoo@users.noreply.github.com>
2026-07-20 21:30:49 +0300

    Finsh Domain Layer

---
+++ Career-Pilot-iOS/Modules/PracticeSession/Presentation/VM/PracticeSessionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PracticeSessionViewModel.swift
+//  Career-Pilot-iOS
+//
+//  Created by Mohamed Magdy on 20/07/2026.
+//
+
+import Foundation
---
+++ Career-Pilot-iOS/Modules/PracticeSession/Presentation/View/PracticeSessionView.swift
@@ -0,0 +1,20 @@
+//
+//  PracticeSessionView.swift
+//  Career-Pilot-iOS
+//
+//  Created by Mohamed Magdy on 20/07/2026.
+//
+
+import SwiftUI
+
+struct PracticeSessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PracticeSessionView_Previews: PreviewProvider {
+    static var previews: some View {
+        PracticeSessionView()
+    }
+}
