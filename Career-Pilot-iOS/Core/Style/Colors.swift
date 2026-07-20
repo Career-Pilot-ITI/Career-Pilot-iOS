9a2279de3a8a49e260b1b1625e354b3b438d859e
Moaz Osama <moazosama204@gmail.com>
2026-07-19 18:13:34 -0700

    add new styles for styles folder

--- Career-Pilot-iOS/Core/Style/Colors.swift
+++ Career-Pilot-iOS/Core/Style/Colors.swift
@@ -63,10 +63,8 @@ extension Color {
    static let primaryTeal = Color("Teal")
    static let primaryTealLight = Color("TealLight")
    static let primaryNavy = Color("PrimaryNavy")
-    
-
-    
-  }
+   static let tealAccent = Color(red: 0.31, green: 0.86, blue: 0.76)
+}
 
 
 
