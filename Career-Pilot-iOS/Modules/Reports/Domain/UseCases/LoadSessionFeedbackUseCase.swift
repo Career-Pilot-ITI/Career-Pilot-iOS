da0f35c16c97c872e91c2cb1bd79d9a7264c940a
Moaz Osama <moazosama204@gmail.com>
2026-07-26 06:27:07 -0700

    finish domain layer

---
+++ Career-Pilot-iOS/Modules/Reports/Domain/UseCases/LoadSessionFeedbackUseCase.swift
@@ -0,0 +1,25 @@
+//
+//  LoadSessionFeedbackUseCase.swift
+//  Career-Pilot-iOS
+//
+//  Created by Moaz on 26/07/2026.
+//
+
+import Foundation
+
+struct LoadSessionFeedbackInput {
+    let sessionId: Int
+    let forceRefresh: Bool
+}
+
+final class LoadSessionFeedbackUseCase: UseCase {
+    private let repository: ReportsRepositoryProtocol
+
+    init(repository: ReportsRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    func execute(_ input: LoadSessionFeedbackInput) async throws -> SessionFeedbackDTO {
+        try await repository.loadFeedback(sessionId: input.sessionId, forceRefresh: input.forceRefresh)
+    }
+}
