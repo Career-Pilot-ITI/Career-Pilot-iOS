e086592b27a91c52c064add261b2b7a9293190b1
AhmedElSayyadMohamed <elsayyada197@gmail.com>
2026-08-09 16:19:02 +0300

    feat(interviews): add interview track models

--- Career-Pilot-iOS/Modules/Interviews/Model/InterviewTrackItem.swift
+++ Career-Pilot-iOS/Modules/Interviews/Model/InterviewTrackItem.swift
@@ -5,4 +5,49 @@
 //  Created by Ahmed El-Sayyad Mohamed on 09/08/2026.
 //
 
-import Foundation
+import SwiftUI
+
+enum InterviewLevel: String {
+    case junior = "Junior"
+    case mid = "Mid"
+    case senior = "Senior"
+    
+    var duration: Int {
+        switch self {
+        case .junior: return 20
+        case .mid: return 40
+        case .senior: return 60
+        }
+    }
+    
+    var levelColor : Color {
+        switch self {
+        case .junior :
+            return .successColour
+        case .mid :
+            return .activeColour
+        case .senior :
+            return .errorColour
+        }
+    }
+    
+    var durationText: String {
+        "~\(duration) min"
+    }
+}
+
+struct InterviewTrack {
+    let track: Track
+    let iconName: String
+    let iconColor: Color
+    let iconBackground: Color
+    let tag: String?            
+    let tagColor: Color
+}
+
+
+struct InterviewItem: Identifiable {
+    let id = UUID()
+    let trackInterview : InterviewTrack
+    let level: InterviewLevel
+}
