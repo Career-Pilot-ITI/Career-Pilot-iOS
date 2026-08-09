7ff7f4f09e368f32975e5ba66dfe6bfd718de018
Mohamed Magdy <88778969+mohamedmagdyoo@users.noreply.github.com>
2026-08-09 16:53:46 +0300

    Update ProfileCard.swift

--- Career-Pilot-iOS/Modules/Settings/Presentation/View/Settings/ProfileCard.swift
+++ Career-Pilot-iOS/Modules/Settings/Presentation/View/Settings/ProfileCard.swift
@@ -17,7 +17,7 @@ struct ProfileCard: View {
 
             }
             else{
-                Image("PersonIcon").resizable().scaledToFill().frame(width: 40, height: 40).clipShape(Circle())
+                Image(user.avatarUrl).resizable().scaledToFill().frame(width: 40, height: 40).clipShape(Circle())
             }
             VStack (alignment:.leading){
                 Text("\(user.fullName)").font(.size16Bold).foregroundColor(.primaryNavy)
