e10bdd669be9db053a1689a5e1917752ec8f76f2
AhmedElSayyadMohamed <elsayyada197@gmail.com>
2026-08-11 18:29:59 +0300

    serperate subviews into seperated files

---
+++ Career-Pilot-iOS/Modules/ATS/Presentation/View/Components/WrapChips.swift
@@ -0,0 +1,18 @@
+//
+//  WrapChips.swift
+//  Career-Pilot-iOS
+//
+//  Created by Ahmed El-Sayyad Mohamed on 11/08/2026.
+//
+
+import SwiftUI
+
+struct WrapChips: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WrapChips()
+}
