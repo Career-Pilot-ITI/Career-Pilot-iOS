a67eeafbedfb7af4a8524d3f868710b74664962f
Ahmed_El_Sayyad <74414358+AhmedElSayyadMohamed@users.noreply.github.com>
2026-08-17 13:19:20 +0300

    Fix typo in UserDataEndPoint audio case

--- Career-Pilot-iOS/Core/Data/Network/EndPoint/Generic/UserDataEndPoint.swift
+++ Career-Pilot-iOS/Core/Data/Network/EndPoint/Generic/UserDataEndPoint.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum FileTypes: String {
-    case CVs = "cvs", Avatars = "avatars", Resumes = "resumes", Audio = "audios"
+    case CVs = "cvs", Avatars = "avatars", Resumes = "resumes", Audio = "audio"
 }
 
 enum UserDataEndpoints: APIEndpoint {
